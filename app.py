bb5902bbd3f7d25f199858ccd5ae59e6ee3c1e35
tonnon <lucastonnon@gmail.com>
2025-08-24 23:36:10 -0300

    deploy

--- app.py
+++ app.py
@@ -287,4 +287,4 @@ def get_token(crew_id):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
